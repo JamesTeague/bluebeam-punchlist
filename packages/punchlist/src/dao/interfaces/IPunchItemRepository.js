721d0a6e812970265bac95d69f1324c9a9bfaac9
James Teague II <jtteague13@gmail.com>
2019-11-04 08:05:54 -0600

    fixing up error

--- packages/punchlist/src/dao/interfaces/IPunchItemRepository.js
+++ packages/punchlist/src/dao/interfaces/IPunchItemRepository.js
@@ -5,8 +5,8 @@ import type { PunchItemStatus } from '../../enums/PunchItemStatusEnum';
 
 export interface IPunchItemRepository {
   getPunchItemById(itemId: string): ?PunchItem;
-  getPunchItemsByProjectId(projectId: string, skip: ?number, limit: ?number,  assigneeId?: ?string, status?: ?PunchItemStatus, sort?: SortType): PunchItem[];
-  getPunchItemsByAssigneeId(assigneeId: string, skip: ?number, limit: ?number,  assigneeId?: ?string, status?: ?PunchItemStatus, sort?: SortType): PunchItem[];
-  getPunchItemsByStatus(status: PunchItemStatus, skip: ?number, limit: ?number,  assigneeId?: ?string, status?: ?PunchItemStatus, sort?: SortType): PunchItem[];
+  getPunchItemsByProjectId(projectId: string, skip: ?number, limit: ?number, assigneeId?: ?string, status?: ?PunchItemStatus, sort?: SortType): PunchItem[];
+  getPunchItemsByAssigneeId(assigneeId: string, skip: ?number, limit: ?number, sort?: SortType): PunchItem[];
+  getPunchItemsByStatus(status: PunchItemStatus, skip: ?number, limit: ?number, sort?: SortType): PunchItem[];
   addOrUpdatePunchItem(punchItem: PunchItem): void;
 }
